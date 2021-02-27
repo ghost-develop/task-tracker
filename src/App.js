220df9e5b73556206712a7d33da74e06334ad06a
Casey Calkins <c.angelcalkins@outlook.com>
2021-02-27 14:52:37 -0600

    Added tasks and set the task list as global state so I can pass them as props to other components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import Header from './components/Header';
+import Tasks from './components/Tasks'
+import {useState} from 'react';
 
 function App() {
+  const [tasks, setTask] = useState([
+    {
+        id: 1,
+        text: 'Doctors appointment',
+        day: 'Feb 5th at 2:30',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Meeting at school',
+        day: 'Feb 6th at 1:30',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Food shopping',
+        day: 'Feb 5th at 2:30',
+        reminder: false,
+    }, 
+]);
+
   return (
     <div className="container">
       <Header />
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
